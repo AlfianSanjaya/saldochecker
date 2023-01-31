fbac90f1415f7b5bd739c2e050ffa7dc6586cbed
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2023-01-31 21:22:27 +0100

    Create basic layout of form

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SaldoChecker());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SaldoChecker extends StatelessWidget {
+  const SaldoChecker({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SaldoChecker App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -19,10 +19,46 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Saldochecker"),
         ),
-        body: const Center(
-          child: Text("Hello world!"),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            margin: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Container(
+                  child: const Text(
+                    'Nummer vervoerbewijs op elektronische kaart',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: 500,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'D0021B698F5BBC2C',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: onPressed,
+                  child: const Text('Check'),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  void onPressed() {
+    // TODO: Implement OnPressed function
   }
 }
